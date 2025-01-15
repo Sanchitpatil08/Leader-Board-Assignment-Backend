14649828f3706a467f97b35611ac70b52fd1ed38
Sanchit <sanchitpatil81004@gmail.com>
2025-01-16 02:38:36 +0530

    Updated the get route
    Updated the get route

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get('/', (req,res)=> res.send("Welcome to Backend"))
 app.use('/api/users', userRoutes);
 app.use('/api/points', pointsRoutes);
 
